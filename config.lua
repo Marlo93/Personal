794464ae8d58ea73e7ecc0e81c0bb9455a339d4b
Marlo <90514479+Marlo93@users.noreply.github.com>
2022-05-29 23:57:21 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -17,5 +17,6 @@ Config = {
     -- Catégorie menu F5.
     nameServer = 'Name Server', -- Nom du menu principal, de préférence mettez le nom du serveur.
     haveNotweaponItem = true, -- Si vous avez les armes en items, mettez sur false / Si vous n'avez pas les armes en item, laissez sur true.
+    showBankWallet = true, -- Si vous souhaitez voir votre argent en banque dans votre portefeuille, mettez sur true, dans le cas contraire, mettez sur false.
     ColorBanner = {r = 0, g = 0, b = 0,}, -- https://htmlcolorcodes.com/fr/
 }
